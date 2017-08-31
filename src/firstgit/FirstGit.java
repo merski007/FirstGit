0c9b3c45a71d33b0779d56210742d74816796a2a
mjmersenski <mjmersenski@10.30.23.158>
2017-08-30 19:47:15 -0500

    removing project startup comments

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package firstgit;
 
-/**
- *
- * @author mjmersenski
- */
 public class FirstGit {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello again");
     }
     
 }
